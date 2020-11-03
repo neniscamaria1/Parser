bf0a73adf25f6f981ce15a41a60692a621bb6d6f
Nenisca Maria <neniscamaria13@gmail.com>
2020-11-03 20:08:37 +0200

    final

--- src/Main.java
+++ src/Main.java
@@ -50,16 +50,16 @@ class Main {
                     finished=true;
                     break;
                 case 1:
-                    System.out.println(fa_const_string.getSetOfStates());
+                    System.out.println(fa_id.getSetOfStates());
                     break;
                 case 2:
-                    System.out.println(fa_const_string.getAlphabet());
+                    System.out.println(fa_id.getAlphabet());
                     break;
                 case 3:
-                    System.out.println(fa_const_string.getInitialState());
+                    System.out.println(fa_id.getInitialState());
                     break;
                 case 4:
-                    System.out.println(fa_const_string.getFinalStates());
+                    System.out.println(fa_id.getFinalStates());
                     break;
                 case 5:
                     fa_id.getTransitions().forEach(System.out::println);
@@ -68,7 +68,7 @@ class Main {
                     System.out.println("Sequence: ");
                     console.nextLine();
                     String sequence = console.nextLine();
-                    System.out.println(fa_const_string.isSequenceAccepted(sequence));
+                    System.out.println(fa_id.isSequenceAccepted(sequence));
                     break;
                 default:
                     System.out.println("Wrong command");
